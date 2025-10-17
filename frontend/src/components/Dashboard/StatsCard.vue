db4c86b929fd94244ee662db98e6bc08de84b5d0
Zohade <zohadestyve@gmail.com>
2025-10-17 12:59:12 +0100

    feature/front/ init dashboard and redirecction

--- frontend/src/components/Dashboard/StatsCard.vue
+++ frontend/src/components/Dashboard/StatsCard.vue
@@ -3,12 +3,14 @@
     <div class="lg:flex lg:justify-between flex-col">
       <div class="flex space-x-2">
         <input
+          @click.stop="active"
           v-if="!checkModuleStatus"
           id="default-checkbox"
           type="checkbox"
           class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
         />
         <input
+          @click.stop="deactive"
           v-else
           id="default-checkbox"
           type="checkbox"
