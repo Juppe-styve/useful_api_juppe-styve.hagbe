d12c077aab5b7558e64f11bc1bc91472c4e539d3
Zohade <zohadestyve@gmail.com>
2025-10-17 12:54:00 +0100

    feature/front/ init dashboard and redirecction

--- frontend/src/components/Dashboard/StatsCard.vue
+++ frontend/src/components/Dashboard/StatsCard.vue
@@ -3,15 +3,24 @@
     <div class="lg:flex lg:justify-between flex-col">
       <div class="flex space-x-2">
         <input
+          v-if="!checkModuleStatus"
           id="default-checkbox"
           type="checkbox"
+          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+        />
+        <input
+          v-else
+          id="default-checkbox"
+          type="checkbox"
+          checked
           class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
         />
         <div class="text-bleck text-sm font-medium">{{ module.name }}</div>
       </div>
       <p class="text-gray-500 text-sm font-medium">{{ module.description }}</p>
     </div>
-    <div class="text-sm font-medium mt-3 text-green-500">actif</div>
+    <div v-if="!checkModuleStatus" class="text-sm font-medium mt-3 text-red-500">inactif</div>
+    <div v-else class="text-sm font-medium mt-3 text-green-500">actif</div>
   </div>
 </template>
 
@@ -20,5 +29,16 @@ import { computed, defineProps } from 'vue'
 const props = defineProps({
   module: Object,
   user_modules: Object,
+})
+
+const checkModuleStatus = computed(() => {
+  const find = props.user_modules[0].find((module) => module.module_id == props.module.id)
+  if (find) {
+    if (find.active == true) {
+      return true
+    }
+    return false
+  }
+  return false
 })
 </script>
